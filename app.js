42ec5e3a68f210d607f3071fee6f4e7a75783513
Anand Jee Choubey <anand_j@es.iitr.ac.in>
2020-10-20 19:04:07 +0530

    Last Commit

--- app.js
+++ app.js
@@ -1,14 +1,61 @@
+require('dotenv').config();
+
 var express = require("express"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override");
 
 var app = express();
+var Responses = require("./responseSchema");
 
+app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.DB_Url , { useNewUrlParser: true });
+
 app.get('/',function(req,res){
     res.render("form");
+});
+
+app.get('/responses',function(req, res){
+    Responses.find({}, function(err, allResp){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("responses",{msg: "Responses will be shown here soon!", allResp : allResp})
+        }
+    });
+});
+
+app.get('/login',function(req, res){
+    res.send("This can be made a login route!")
+});
+
+app.get('/register',function(req, res){
+    res.send("This can be made a user registration route!")
+});
+
+app.post('/',function(req,res){
+    Responses.create(req.body,function(err,newResponse){
+        if(err){
+            res.send(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+app.delete('/responses/:id',function(req,res){
+    Responses.findByIdAndRemove(req.params.id, function(err, foundPost){
+        if(err){
+            res.send(err);
+        } else {
+            res.redirect("/responses");
+        }
+    });
 });
 
 const port = process.env.PORT || 3000 ;
--- public/landing.js
+++ public/landing.js
@@ -1,3 +1,5 @@
+document.getElementById("tnc").required = true;
+
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
