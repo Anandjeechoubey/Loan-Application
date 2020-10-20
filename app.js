1bc60859fc918e61254fc7ae38bf2b9e037cc39a
Anand Jee Choubey <anand_j@es.iitr.ac.in>
2020-10-20 19:34:43 +0530

    Final Commit

--- app.js
+++ app.js
@@ -14,20 +14,24 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.DB_Url , { useNewUrlParser: true });
+mongoose.connect(process.env.DB_Url , { useNewUrlParser: true, useFindAndModify: false });
 
 app.get('/',function(req,res){
     res.render("form");
 });
 
-app.get('/responses',function(req, res){
-    Responses.find({}, function(err, allResp){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("responses",{msg: "Responses will be shown here soon!", allResp : allResp})
-        }
-    });
+app.get("/responses", async (req, res, next) => {
+    try {
+        Responses.find({}, function(err, allResp){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("responses",{msg: "Responses will be shown here soon!", allResp : allResp})
+            }
+        });
+    } catch (err) {
+      next(err);
+    }
 });
 
 app.get('/login',function(req, res){
@@ -38,24 +42,32 @@ app.get('/register',function(req, res){
     res.send("This can be made a user registration route!")
 });
 
-app.post('/',function(req,res){
-    Responses.create(req.body,function(err,newResponse){
-        if(err){
-            res.send(err);
-        } else {
-            res.redirect("/");
-        }
-    });
+app.post("/", async (req, res, next) => {
+    try {
+        Responses.create(req.body,function(err,newResponse){
+            if(err){
+                res.send(err);
+            } else {
+                res.redirect("/");
+            }
+        });
+    } catch (err) {
+      next(err);
+    }
 });
 
-app.delete('/responses/:id',function(req,res){
-    Responses.findByIdAndRemove(req.params.id, function(err, foundPost){
-        if(err){
-            res.send(err);
-        } else {
-            res.redirect("/responses");
-        }
-    });
+app.delete("/responses/:id", async (req, res, next) => {
+    try {
+        Responses.findByIdAndRemove(req.params.id, function(err, foundPost){
+            if(err){
+                res.send(err);
+            } else {
+                res.redirect("/responses");
+            }
+        });
+    } catch (err) {
+      next(err);
+    }
 });
 
 const port = process.env.PORT || 3000 ;
